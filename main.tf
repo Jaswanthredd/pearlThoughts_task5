545b12b7d5e8c186caa445a42595bae1b7cb16c7
Jaswanth Reddy <94891769+Jaswanthredd@users.noreply.github.com>
2024-09-18 15:58:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,46 +5,25 @@ provider "aws" {
 resource "aws_instance" "medusa_ec2" {
   ami           = "ami-0e86e20dae9224db8"  # Use a valid AMI ID for your region
   instance_type = "t2.small"  # Choose your instance type
-  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+  # If using a key pair for SSH (not used here but may be necessary if you decide to use SSH)
+  # key_name = "your-key-name"
+
+  # Configure security group inline
+  vpc_security_group_ids = [aws_security_group.medusa_sg.id]
+
+  # User data to set up the instance
+  user_data = <<-EOF
+                #!/bin/bash
+                apt-get update -y
+                apt-get install -y docker.io
+                systemctl start docker
+                docker run -d -p 80:80 medusa_image:latest
+                EOF
 
   tags = {
     Name = "MedusaEC2Instance"
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              sudo apt install -y docker.io
-              sudo systemctl start docker
-              sudo docker run -d -p 80:80 medusa_image:latest
-              EOF
-}
-
-resource "aws_iam_role" "ssm_role" {
-  name = "SSMRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com",
-        },
-        Action = "sts:AssumeRole",
-      },
-    ],
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  role     = aws_iam_role.ssm_role.name
-}
-
-resource "aws_iam_instance_profile" "ssm_profile" {
-  name = "SSMInstanceProfile"
-  role = aws_iam_role.ssm_role.name
 }
 
 resource "aws_security_group" "medusa_sg" {
